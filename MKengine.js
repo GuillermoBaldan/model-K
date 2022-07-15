30da0b0f2fa30886e7422da8893192db1910b82e
GuillermoBaldan <guillermobaldan@gmail.com>
2022-07-15 05:42:34 +0200

    Some functions added

--- MKengine.js
+++ MKengine.js
@@ -1,15 +1,27 @@
-let expression = "car is object";
+let expression = "car is system?";
 
 let database = {};
+
+let patternArray = ["[a-zA-Z]* is (object|property|sytem|)","[a-zA-Z]* is (object|property|sytem|)[?]"]
 
 function MKparserv1(expression){
 
 }
 
 function MKvalidatorV1(expression){
-let patter = /[a-zA-Z]* is (object|property|sytem|)/;
-return patter.test(expression)
+let pattern = /[a-zA-Z]* is (object|property|sytem|)/;
+return pattern.test(expression)
 
 }
 
-console.log(MKvalidatorV1(expression));
+function MKvalidatorV2(expression,patternArray){
+let flag = false;
+patternArray.forEach(pattern => {
+    if(new RegExp(pattern, "").test(expression)){
+        flag = true
+    }
+})
+return flag;
+}
+console.log(patternArray)
+console.log(`expression: ${expression} is ${MKvalidatorV2(expression,patternArray)}`)
