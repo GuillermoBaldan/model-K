62bb1ede99e4d2ae82177a52c8c9b2660fda7fbe
GuillermoBaldan <guillermobaldan@gmail.com>
2022-07-18 15:42:59 +0200

    delete some files of tests

--- tests/unit-testing/patternOperatorArray.js
+++
@@ -1,3 +0,0 @@
-const patternOperatorArray = ["[a-zA-Z]* is (object|property|system)(?![?])"];
-
-module.exports = patternOperatorArray;
--- tests/unit-testing/patternQueryArray.js
+++
@@ -1,3 +0,0 @@
-const patternQueryArray = ["[a-zA-Z]* is (object|property|system)(?![?])"];
-
-module.exports = patternQueryArray;
